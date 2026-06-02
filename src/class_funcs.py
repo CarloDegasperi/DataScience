87d100882304c0c15ffa6b31fee6e62705cfe48c
CarloDegasperi <carlodegasperi04@gmail.com>
2026-06-02 21:09:00 +0200

    fine parte binaria

--- src/class_funcs.py
+++ src/class_funcs.py
@@ -1,12 +1,24 @@
 import pandas as pd
 import numpy as np
+from sklearn import metrics
 
-def get_AQI_inq(value, intervals):
-    if pd.isna(value):
-        return np.nan
+def get_performance_param(model, X, y, thr = 0.5):
 
-    for i, (low, high) in enumerate(intervals):
-        if low <= value < high:
-            return 25 / (high - low) * (value - low) + 20 * i
-               
-    return np.nan
+    y_proba = model.predict_proba(X)[:, 1]
+    y_pred = (y_proba >= thr).astype(int)
+
+    accuracy_score = metrics.accuracy_score(y, y_pred)
+    precision_score = metrics.precision_score(y, y_pred)
+    recall_score = metrics.recall_score(y, y_pred)
+    f1_score = metrics.f1_score(y, y_pred)
+    MCC_score = metrics.matthews_corrcoef(y, y_pred)
+
+    print(f'Accuratezza: {accuracy_score:.5f}')
+    print(f'Precisione: {precision_score:.5f}')
+    print(f'Recall: {recall_score:.5f}')
+    print(f'F1 score: {f1_score:.5f}')
+    print(f'MCC: {MCC_score:.5f}')
+
+    return None
+
+
