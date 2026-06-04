97bf2438be53d5528abeebf5e1a4a1d5a6dd5409
CarloDegasperi <carlodegasperi04@gmail.com>
2026-06-04 15:00:33 +0200

    classificazione fino alle 5 classi comprese

--- src/class_funcs.py
+++ src/class_funcs.py
@@ -85,23 +85,38 @@ def display_confusion_matrix(model, title1, X1, y1, title2 = None, X2 = None, y2
 
 ################################################################################################################################################
 
-def get_performance_param(model, X, y, thr = 0.5):
+def get_performance_param(model, X, y, binary = True, thr = 0.5):
 
-    y_proba = model.predict_proba(X)[:, 1]
-    y_pred = (y_proba >= thr).astype(int)
+    if binary:
+        y_proba = model.predict_proba(X)[:, 1]
+        y_pred = (y_proba >= thr).astype(int)
 
-    accuracy_score = metrics.accuracy_score(y, y_pred)
-    precision_score = metrics.precision_score(y, y_pred)
-    recall_score = metrics.recall_score(y, y_pred)
-    f1_score = metrics.f1_score(y, y_pred)
-    MCC_score = metrics.matthews_corrcoef(y, y_pred)
+        accuracy_score = metrics.accuracy_score(y, y_pred)
+        precision_score = metrics.precision_score(y, y_pred)
+        recall_score = metrics.recall_score(y, y_pred)
+        f1_score = metrics.f1_score(y, y_pred)
+        MCC_score = metrics.matthews_corrcoef(y, y_pred)
 
-    print(f'Accuratezza: {accuracy_score:.5f}')
-    print(f'Precisione: {precision_score:.5f}')
-    print(f'Recall: {recall_score:.5f}')
-    print(f'F1 score: {f1_score:.5f}')
-    print(f'MCC: {MCC_score:.5f}')
+        print(f'Accuratezza:    {accuracy_score:.5f}')
+        print(f'Precisione:     {precision_score:.5f}')
+        print(f'Recall:         {recall_score:.5f}')
+        print(f'F1 score:       {f1_score:.5f}')
+        print(f'MCC:            {MCC_score:.5f}')
+    
+    else:
+        y_pred = model.predict(X)
+        accuracy_score = metrics.accuracy_score(y, y_pred)
+        precision_score = metrics.precision_score(y, y_pred, average='weighted')
+        recall_score = metrics.recall_score(y, y_pred, average='weighted')
+        f1_score = metrics.f1_score(y, y_pred, average='weighted')
+        MCC_score = metrics.matthews_corrcoef(y, y_pred)
 
+        print(f'Accuratezza:    {accuracy_score:.5f}')
+        print(f'Precisione:     {precision_score:.5f}')
+        print(f'Recall:         {recall_score:.5f}')
+        print(f'F1 score:       {f1_score:.5f}')
+        print(f'MCC:            {MCC_score:.5f}')
+        
     return None
 
 ################################################################################################################################################
