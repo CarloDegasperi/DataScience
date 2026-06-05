e768dd5f6f0fa134d74633b3a3cb6a84ecfd33e5
CarloDegasperi <carlodegasperi04@gmail.com>
2026-06-05 23:08:39 +0200

    main fatto bene fino alla prima parte della classificazione con random forest

--- src/class_funcs.py
+++ src/class_funcs.py
@@ -120,3 +120,55 @@ def get_performance_param(model, X, y, binary = True, thr = 0.5):
     return None
 
 ################################################################################################################################################
+
+def display_confusion_matrix_multiclass(model, class_order, title1, X1, y1, title2 = None, X2 = None, y2 = None):
+    if X2 is None and y2 is None:
+        plt.figure(figsize=(12, 5))
+
+        y_pred = model.predict(X1)
+        metrics.ConfusionMatrixDisplay.from_predictions(y1, y_pred, labels=class_order, display_labels=class_order)
+        plt.title(title1)
+
+    else:
+        fig, axes = plt.subplots(1, 2, figsize=(12, 5))
+
+        y_pred1 = model.predict(X1)
+        metrics.ConfusionMatrixDisplay.from_predictions(y1, y_pred1, labels=class_order, display_labels=class_order, ax = axes[0])
+        axes[0].set_title(title1)
+
+        y_pred2 = model.predict(X2)
+        metrics.ConfusionMatrixDisplay.from_predictions(y2, y_pred2, labels=class_order, display_labels=class_order, ax = axes[1])
+        axes[1].set_title(title2)
+
+        plt.tight_layout()
+        plt.show()
+
+################################################################################################################################################
+
+def get_AQI_pol(value, intervals):
+    if pd.isna(value):
+        return np.nan
+
+    for i, (low, high) in enumerate(intervals):
+        if high == np.inf:
+            if value >= low:
+                return 20 * (i + 1)
+        if low <= value < high:
+            return 20 / (high - low) * (value - low) + 20 * i
+               
+    return np.nan
+
+################################################################################################################################################
+
+def display_importances(model, feature_cols):
+    importance_df = pd.DataFrame({'importance': model.feature_importances_}, index=feature_cols)
+
+    importance_df = importance_df.sort_values(by='importance', ascending=True)
+
+    importance_df.plot.barh(figsize=(9,7))
+
+    plt.xlabel('Feature Importance')
+    plt.title('Random Forest Feature Importance')
+    plt.grid()
+    plt.show()
+
