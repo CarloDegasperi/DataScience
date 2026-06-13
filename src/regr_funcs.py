6b8ff5a967ee90874d245b6d262a48f709824a7c
CarloDegasperi <carlodegasperi04@gmail.com>
2026-06-13 12:57:01 +0200

    fine modifiche individuali regressione

--- src/regr_funcs.py
+++ src/regr_funcs.py
@@ -92,16 +92,14 @@ def plot_predictions(model, X1, y1, X2, y2):
 
         station_data_tr = graph1_df[graph1_df[sta] == 1]
         station_data_te = graph2_df[graph2_df[sta] == 1]
-        y_true = station_data_te["actual"]
-        y_pred = station_data_te["prediction"]
 
         if station_data_tr.empty:
             continue
 
         sta_name = sta.removeprefix('station_')
 
-        rmse = np.sqrt(metrics.mean_squared_error(y_true, y_pred))
-        r2 = metrics.r2_score(y_true, y_pred)
+        rmse = np.sqrt(metrics.mean_squared_error(station_data_te["actual"], station_data_te["prediction"]))
+        r2 = metrics.r2_score(station_data_te["actual"], station_data_te["prediction"])
 
         ax = axes[i]
         i += 1
@@ -124,3 +122,117 @@ def plot_predictions(model, X1, y1, X2, y2):
     plt.show()
 
 ################################################################################################################################################
+
+def plot_predictions_points(model, X1, y1, X2, y2):
+
+    graph1_df = X1.copy()
+    graph1_df['actual'] = y1
+    graph1_df['prediction'] = model.predict(X1)
+    graph1_df['time'] = (graph1_df['day'])
+
+    graph2_df = X2.copy()
+    graph2_df['actual'] = y2
+    graph2_df['prediction'] = model.predict(X2)
+    graph2_df['time'] = (graph2_df['day'])
+
+    fig, axes = plt.subplots(3, 2, figsize=(16, 16))
+    axes = axes.flatten()
+
+    stations = [col for col in graph1_df.columns if col.startswith('station_')]
+
+    i = 0
+
+    for sta in stations:
+
+        station_data_tr = graph1_df[graph1_df[sta] == 1]
+        station_data_te = graph2_df[graph2_df[sta] == 1]
+
+        if station_data_tr.empty:
+            continue
+
+        sta_name = sta.removeprefix('station_')
+
+        rmse = np.sqrt(metrics.mean_squared_error(station_data_te["actual"], station_data_te["prediction"]))
+        r2 = metrics.r2_score(station_data_te["actual"], station_data_te["prediction"])
+
+        ax = axes[i]
+        i += 1
+
+        sns.scatterplot(data=station_data_tr, x='time', y='actual', color='blue', label='actual in train', ax=ax)
+        sns.scatterplot(data=station_data_tr, x='time', y='prediction', color='red', label='prediction in train', ax=ax)
+        
+        sns.scatterplot(data=station_data_te, x='time', y='actual', color='green', label='actual in test', ax=ax)
+        sns.scatterplot(data=station_data_te, x='time', y='prediction', color='orange', label='prediction in test', ax=ax)
+        
+
+        ax.set_title(f'Dati di {sta_name}\nRMSE_test={rmse:.2f}  R²_test={r2:.3f}')
+        ax.set_ylabel('concentration [µg/m³]')
+        ax.grid(True)
+        ax.legend()
+
+    for ax in axes[i:]:
+        ax.set_visible(False)
+
+    plt.tight_layout()
+    plt.show()
+
+################################################################################################################################################
+
+def plot_comparison_points(model, model_log1p, X1, y1, X2, y2):
+
+    graph1_df = X1.copy()
+    graph1_df['actual'] = y1
+    graph1_df['prediction'] = model.predict(X1)
+    graph1_df['prediction_log1p'] = np.exp(model_log1p.predict(X1)) - 1
+    graph1_df['time'] = (graph1_df['day'])
+
+    graph2_df = X2.copy()
+    graph2_df['actual'] = y2
+    graph2_df['prediction'] = model.predict(X2)
+    graph2_df['prediction_log1p'] = np.exp(model_log1p.predict(X2)) - 1
+    graph2_df['time'] = (graph2_df['day'])
+
+    fig, axes = plt.subplots(3, 2, figsize=(16, 16))
+    axes = axes.flatten()
+
+    stations = [col for col in graph1_df.columns if col.startswith('station_')]
+
+    i = 0
+
+    for sta in stations:
+
+        station_data_tr = graph1_df[graph1_df[sta] == 1]
+        station_data_te = graph2_df[graph2_df[sta] == 1]
+
+        if station_data_tr.empty:
+            continue
+
+        sta_name = sta.removeprefix('station_')
+
+        rmse = np.sqrt(metrics.mean_squared_error(station_data_te['actual'], station_data_te['prediction']))
+        rmse_log1p = np.sqrt(metrics.mean_squared_error(station_data_te['actual'], station_data_te['prediction_log1p']))
+        r2 = metrics.r2_score(station_data_te['actual'], station_data_te['prediction'])
+        r2_log1p = metrics.r2_score(station_data_te['actual'], station_data_te['prediction_log1p'])
+
+        ax = axes[i]
+        i += 1
+
+        sns.scatterplot(data=station_data_tr, x='time', y='actual', color='black', label='actual in train', ax=ax)
+        sns.scatterplot(data=station_data_tr, x='time', y='prediction', color='red', label='prediction in train', ax=ax)
+        sns.scatterplot(data=station_data_tr, x='time', y='prediction_log1p', color='blue', label='prediction log1p in train', ax=ax)
+        
+        sns.scatterplot(data=station_data_te, x='time', y='actual', color='black', label='actual in test', ax=ax)
+        sns.scatterplot(data=station_data_te, x='time', y='prediction', color='orange', label='prediction in test', ax=ax)
+        sns.scatterplot(data=station_data_te, x='time', y='prediction_log1p', color='green', label='prediction log1p in test', ax=ax)
+        
+
+        ax.set_title(f'Dati di {sta_name}\nRMSE_test={rmse:.2f}  R²_test={r2:.3f}\nlog1p: RMSE_test={rmse_log1p:.2f}  R²_test={r2_log1p:.3f}')
+        ax.set_ylabel('concentration [µg/m³]')
+        ax.grid(True)
+        ax.legend()
+
+    for ax in axes[i:]:
+        ax.set_visible(False)
+
+    plt.tight_layout()
+    plt.show()
